98dda7f1f2f57d10994ef252e4f6bd13c5dcc994
John Swindin <johnswindin@gmail.com>
2017-06-19 19:04:59 +0100

    modified app.js to respond with view files. Added view files

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 
+// basic static requests... 
 app.get('/', (req, res) => {
-  res.send('This is the home page')
+  res.sendFile('views/index.html')
 })
 
 app.get('/contact', (req, res) => {
-  res.send('This is the contact page')
+  res.sendFile('views/contact.html')
 })
 
+app.get('/about', (req, res) => {
+  res.sendFile('views/about.html')
+})
+
+// example of a dynamic request... 
+app.get('/profile/:id', (req, res) => {
+  res.send('You requested the profile with the id of: ' + req.params.id)
+})
 app.listen(3000)
