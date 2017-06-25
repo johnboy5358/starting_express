79991e75f3b3a5f4d90af21b8bd6bb8ed5c46e0a
John Swindin <johnswindin@gmail.com>
2017-06-25 14:49:58 +0100

    contact form for selected team member and post to server for action.

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+// create application/x-www-form-urlencoded parser 
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 const app = express()
 const dbBoys = [
-  {"id": '0', "name": "Hamish", "age": 270, "gender": "male", "comments": "Professor of Astro Physics, Vulcanology and Astronautics. Currently, Astrophysicist engaged in 2022 Man to Mars Mission."},
-  {"id": '1', "name": "Bramley", "age": 150, "gender": "male", "comments": "Michelin Star Chef, specializing in Bramley Apple deserts."},
-  {"id": '2', "name": "Hannibal", "age": 2264, "gender": "male", "comments": "World renowned explorer, specialist in apline travel."},
-  {"id": '3', "name": "Barnaby", "age": 189, "gender": "male", "comments": "World renowned Morris Dancing bear."},
-  {"id": '4', "name": "Benjy", "age": 215, "gender": "male", "comments": "Expert in all yoga disciplines"}
+  {"id": '0', "name": "Hamish", "email": "hamish@heronomus.com", "age": 270, "gender": "male", "comments": "Professor of Astro Physics, Vulcanology and Astronautics. Currently, Astrophysicist engaged in 2022 Man to Mars Mission."},
+  {"id": '1', "name": "Bramley", "email": "bramley@applechefs.net", "age": 150, "gender": "male", "comments": "Michelin Star Chef, specializing in Bramley Apple deserts."},
+  {"id": '2', "name": "Hannibal", "email": "hannibal@armchairexplores.org", "age": 2264, "gender": "male", "comments": "World renowned explorer, specialist in apline travel."},
+  {"id": '3', "name": "Barnaby", "email": "barnaby@morrisdancers.com", "age": 189, "gender": "male", "comments": "World renowned Morris Dancing bear."},
+  {"id": '4', "name": "Benjy", "email": "benjy@flexibility.net", "age": 215, "gender": "male", "comments": "Expert in all yoga disciplines"}
 ]
+let messages = []
 
 // gets a specific profile from dbBoys by id.
 const queryId = id => dbBoys.filter(boy => boy.id === id)
@@ -15,12 +21,26 @@ app.set('view engine', 'pug')
 app.use(express.static('public'))
 
 // basic static requests... 
+app.get('', (req, res) => {
+  res.render('index')
+})
+
 app.get('/home', (req, res) => {
   res.render('index')
 })
 
 app.get('/contact', (req, res) => {
   res.render('contact')
+})
+
+app.post('/profiles', urlencodedParser, (req, res) => {
+  messages.push(req.body)
+  // clear the console.
+  console.log('\033[2J')
+  // show all saved messages.
+  console.log(messages)
+  // tell user ... we are dealing with your message.
+  res.render('response', {name: req.body['your-name'], actionBy: req.body['name'], respond: " reply to your message shortly."})
 })
 
 app.get('/about', (req, res) => {
@@ -47,6 +67,12 @@ app.get('/profiles/:id', (req, res) => {
 app.get('/api/team/:id', (req, res) => {
   res.send({person: queryId(req.params.id)[0]})
 })
+
+app.get('*', function(req, res){
+  console.log("got a 404")
+  res.render('404')
+});
+
 
 app.listen(3000)
 console.log('Your express app is running on localhost:3000')
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,32 +1,71 @@
-const res = document.querySelector('#result');
+(function($){
 
-function getProfile(id){
-  console.log("You clicked me!")
-  const xhttp = new XMLHttpRequest()
-  xhttp.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      const dat = JSON.parse(this.responseText)
-      console.log(dat)
-      const htmlDat = `
-        <table id="profiles">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Age</th>
-              <th>Comments</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>${dat.person.name}</td>
-              <td>${dat.person.age}</td>
-              <td>${dat.person.comments}</td>
-            </tr>
-          </tbody>
-        </table>`
-      document.getElementById("result").innerHTML = htmlDat
+  const res = document.querySelector('#result');
+
+  function getProfile(id){
+    console.log("You clicked me!")
+    const xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const dat = JSON.parse(this.responseText)
+        console.log(dat)
+        const htmlDat = `
+          <table id="profiles">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Comments</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>${dat.person.name}</td>
+                <td>${dat.person.age}</td>
+                <td>${dat.person.comments}</td>
+              </tr>
+            </tbody>
+          </table>`
+        document.getElementById("result").innerHTML = htmlDat
+      }
+    };
+    xhttp.open("GET", "/api/team/"+id, true);
+    xhttp.send();
+  }
+
+  $('#profiles tbody tr').on('click', function(evt){
+    let data = {}
+    let $tds = $(this).find('td')
+    $.each($tds, function () {
+      Object.assign(data, {[$(this).attr('class')]: ($(this).text())})
+    })
+
+    console.log(data)
+
+    $('#profiles tbody tr').each(function () {
+      if($(this).hasClass('selected')) {
+        $(this).removeClass('selected')
+      }
+    })
+
+    if ($(this).hasClass('selected')){
+      $(this).removeClass('selected')
+    } else {
+      $(this).addClass('selected')
     }
-  };
-  xhttp.open("GET", "/api/team/"+id, true);
-  xhttp.send();
-}
+
+    let selected = 0
+    $('#profiles tbody tr').each(function () {
+      if($(this).hasClass('selected')) {
+        selected++
+      }
+    })
+    if (selected>0) {
+      $('#contact-selected').toggleClass('visible')
+    }
+
+    $('#contact-selected #name').val(data.name)
+    $('#contact-selected #email').val(data.email)
+  })
+
+}(jQuery))
